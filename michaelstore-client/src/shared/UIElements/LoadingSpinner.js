87c60a88803f4d67bf21d236abad383fbc135cc9
Osiris <potterosiris@gmail.com>
2021-11-11 10:16:42 -0500

    base

---
+++ michaelstore-client/src/shared/UIElements/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from "react"
+import loading from "../../../img/opaque_spinner.gif"
+
+
+
+const LoadingSpinner = props => {
+    return (
+        <div className={`${props.asOverlay && 'loading-spinner__overlay'}`}>
+            <img src={loading} alt="spinner" />
+        </div>
+    );
+}
+
+export default LoadingSpinner
