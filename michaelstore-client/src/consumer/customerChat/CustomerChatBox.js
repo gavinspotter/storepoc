93e669485407409b36e831f85f7ed87e4f91e05b
Osiris <potterosiris@gmail.com>
2021-11-24 16:43:14 -0500

    grab items in progress

--- michaelstore-client/src/consumer/customerChat/CustomerChatBox.js
+++ michaelstore-client/src/consumer/customerChat/CustomerChatBox.js
@@ -1,274 +1,180 @@
-import React, {useRef, useContext, useEffect, useState} from 'react'
+import React, { useRef, useContext, useEffect, useState } from "react";
 
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { useForm } from "react-hook-form";
 
-import { useHttpClient } from '../../shared/hooks/http-hook'
-import { useForm } from "react-hook-form"
+import { AuthContext } from "../../shared/context/auth-context";
 
-
-import { AuthContext } from '../../shared/context/auth-context'
-
-import ErrorModal from '../../shared/UIElements/ErrorModal'
-import ChatItem from './ChatItem'
-import CustomerItemsList from './CustomerItemsList'
+import ErrorModal from "../../shared/UIElements/ErrorModal";
+import ChatItem from "./ChatItem";
+import CustomerItemsList from "./CustomerItemsList";
 
 const CustomerChatBox = () => {
+  const auth = useContext(AuthContext);
 
-    const auth = useContext(AuthContext)
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-    const { isLoading, error, sendRequest, clearError } = useHttpClient()
-    
-    const { register, handleSubmit, reset,formState: { isSubmitSuccessful }  } = useForm({message: ''})
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful },
+  } = useForm({ message: "" });
 
-    
-
-    useEffect(() => {
-        if (isSubmitSuccessful) {
-          reset({message:""})
-        }
-          
-    },
-    [reset, isSubmitSuccessful])
-
-    const [messages, setMessages] = useState()
-
-    const [mTrigger, setmTrigger] = useState()
-
-
-    const titleRef = useRef()
-
-    const handleBackClick = () => {
-
-        console.log("check")
-        titleRef.current.scrollIntoView({behavior: "smooth"})
-        
-     }
-
-
-    const setTimeScroll = () => {
-        setTimeout(handleBackClick, 1000)
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({ message: "" });
     }
-     
-    
+  }, [reset, isSubmitSuccessful]);
 
-    
+  const [messages, setMessages] = useState();
 
+  const [mTrigger, setmTrigger] = useState();
 
-    
+  const titleRef = useRef();
 
+  const handleBackClick = () => {
+    console.log("check");
+    titleRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-    useEffect(() => {
+  const setTimeScroll = () => {
+    setTimeout(handleBackClick, 1000);
+  };
 
-        const fetchMessages = async () => {
-            try {
-                const responseData = await sendRequest(
-                    `${process.env.REACT_APP_BACKEND_URL}/customer/getMessages`,
-                    "GET",
-                    null,
-                    {
-                        //"Content-Type": "application/json",
-                        Authorization: 'Bearer ' + auth.customerToken 
-                    }
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/customer/getMessages`,
+          "GET",
+          null,
+          {
+            //"Content-Type": "application/json",
+            Authorization: "Bearer " + auth.customerToken,
+          }
+        );
 
-                )
+        setMessages(responseData.findMessageBoard.messages);
 
-                setMessages(responseData.findMessageBoard.messages)
-          
-                //window.scrollTo(0, 99999)
-            } catch (err) {
-                
-            }
+        //window.scrollTo(0, 99999)
+      } catch (err) {}
+    };
+
+    fetchMessages();
+
+    setmTrigger(false);
+  }, [sendRequest, mTrigger, auth.customerToken]);
+
+  const submitAMessage = async (data) => {
+    try {
+      sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/customer/postMessage`,
+        "POST",
+        JSON.stringify({
+          message: data.messages,
+        }),
+        {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.customerToken,
         }
+      );
+    } catch (err) {}
 
+    setmTrigger(true);
 
+    // const fetchMessages = async () => {
+    //     try {
+    //         const responseData = await sendRequest(
+    //             `${process.env.REACT_APP_BACKEND_URL}/customer/getMessages`,
+    //             "GET",
+    //             null,
+    //             {
+    //                 //"Content-Type": "application/json",
+    //                 Authorization: 'Bearer ' + auth.customerToken
+    //             }
 
-        
+    //         )
 
-        fetchMessages();
+    //         setMessages(responseData.findMessageBoard.messages)
 
-        setmTrigger(false)
+    //     } catch (err) {
 
-        
+    //     }
+    // }
 
-       
+    // fetchMessages();
 
-       
-        
+    //
 
-        
+    // handleBackClick()
+  };
 
-    }, [sendRequest, mTrigger, auth.customerToken])
+  const [grabItems, setGrabItems] = useState();
 
-    
-   
-    
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/customer/items`,
+          "GET",
+          null,
+          {
+            //"Content-Type": "application/json",
+            Authorization: "Bearer " + auth.customerToken,
+          }
+        );
 
-    const submitAMessage = async (data) => {
+        setGrabItems(responseData.findConsumerGoods);
+      } catch (err) {}
+    };
 
-        try {
-            sendRequest(
-                `${process.env.REACT_APP_BACKEND_URL}/customer/postMessage`,
-                "POST",
-                JSON.stringify({
-                    message: data.messages,
-                     
-                    
-                }),
-                {
-                    "Content-Type": "application/json",
-                    Authorization: 'Bearer ' + auth.customerToken
+    fetchItems();
+  }, [auth.customerToken, sendRequest]);
 
-                }
-            )
-        } catch (err) {
-            
-        }
+  return (
+    <div className="customerMessage">
+      <ErrorModal error={error} onClear={clearError} />
+      {messages && (
+        <div className="customerMessage-messages">
+          <div className="customerMessage-messages-messages">
+            {<div ref={titleRef}></div>}
 
-        setmTrigger(true)
-        
-
-        
-
-        
-
-
-        // const fetchMessages = async () => {
-        //     try {
-        //         const responseData = await sendRequest(
-        //             `${process.env.REACT_APP_BACKEND_URL}/customer/getMessages`,
-        //             "GET",
-        //             null,
-        //             {
-        //                 //"Content-Type": "application/json",
-        //                 Authorization: 'Bearer ' + auth.customerToken 
-        //             }
-
-        //         )
-
-        //         setMessages(responseData.findMessageBoard.messages)
-               
-        //     } catch (err) {
-                
-        //     }
-        // }
-
-        // fetchMessages();
-
-        // 
-
-        // handleBackClick()
-
-        
-
-        
-
-    }
-
-   
-
-
-
-  
-
- 
-
-
-
-    return (
-        <div className="customerMessage">
-            <ErrorModal
-            error={error} onClear={clearError}
-            />
-            { messages &&
-                
-                <div className="customerMessage-messages">
-            
-                <div  className="customerMessage-messages-messages">
-                
-                
-                
-                
-                
-                {<div ref={titleRef}></div>}
-    
-    
-                {
-
-                    
-                    messages.map(x => 
-    
-                       
-                        
-                        <ChatItem
-                        key={x._id}
-                        sender={x.sender}
-                        message={x.message}
-                        />
-    
-    
-    
-                        
-                        ).reverse()
-
-                        
-                
-    
-    
-                        
-                }
-
-    
-                
-                
-    
-                
-                
-    
-                
-    
-               
-                
-                </div>
-                
-                
-            </div>
-           
-            
-            
-            
-
-            
-
-            
-
-            }
-            <div className="customerMessage-items">
-                <CustomerItemsList/>
-            </div>
-
-
-            
-
-            
-            
-
-            <div className="customerMessage-form-border">
-            
-
-            <form className="customerMessage-form"  onSubmit={handleSubmit(submitAMessage)}>
-
-                <div className="customerMessage-form-form">
-              
-                <button onClick={setTimeScroll} className="customerMessage-form-button">submit</button>
-                <div className="customerMessage-form-textArea">
-                <textarea {...register("messages")} className="customerMessage-form-textArea-input"/>
-                </div>
-                
-                </div>
-
-            </form>
-            </div>
+            {messages
+              .map((x) => (
+                <ChatItem key={x._id} sender={x.sender} message={x.message} />
+              ))
+              .reverse()}
+          </div>
         </div>
-    )
-}
+      )}
+      <div className="customerMessage-items">
+        <CustomerItemsList items={grabItems} />
+      </div>
 
-export default CustomerChatBox
+      <div className="customerMessage-form-border">
+        <form
+          className="customerMessage-form"
+          onSubmit={handleSubmit(submitAMessage)}
+        >
+          <div className="customerMessage-form-form">
+            <button
+              onClick={setTimeScroll}
+              className="customerMessage-form-button"
+            >
+              submit
+            </button>
+            <div className="customerMessage-form-textArea">
+              <textarea
+                {...register("messages")}
+                className="customerMessage-form-textArea-input"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerChatBox;
--- michaelstore-client/src/consumer/customerChat/CustomerItemsList.js
+++ michaelstore-client/src/consumer/customerChat/CustomerItemsList.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import CustomerItemsItem from "./CustomerItemsItem";
 
-const CustomerItemsList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const CustomerItemsList = (props) => {
+  return (
+    <div>
+      {props.items.map((x) => (
+        <CustomerItemsItem key={x._id} id={x._id} name={x.name} />
+      ))}
+    </div>
+  );
+};
 
-export default CustomerItemsList
+export default CustomerItemsList;
