530d7d4521b82f15fe6338b8d58b6e0684e31076
Osiris <potterosiris@gmail.com>
2021-12-01 18:40:19 -0500

    card set up

--- michaelstore-client/src/admin/itemQue/ItemsItem.js
+++ michaelstore-client/src/admin/itemQue/ItemsItem.js
@@ -4,6 +4,8 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 
 import { AuthContext } from "../../shared/context/auth-context";
 
+import { IoPrismOutline } from "react-icons/io5";
+
 const ItemsItem = (props) => {
   const auth = useContext(AuthContext);
 
@@ -24,20 +26,50 @@ const ItemsItem = (props) => {
 
   return (
     <div className="itemsContainer-itemsList-anItem">
-      <div>
-        <div>
-          <img />
-          <h2>{props.name}</h2>
-          <p>{props.description}</p>
+      <div className="itemsContainer-itemsList-anItem-item">
+        <div className="itemsContainer-itemsList-anItem-item-img-imgWrap">
+          <img
+            className="itemsContainer-itemsList-anItem-item-img"
+            alt={props.description}
+            src={`https://s3.us-east-1.amazonaws.com/michaelrossbucket/${props.bucketPhotoId}`}
+          />
         </div>
+        <div className="itemsContainer-itemsList-anItem-item-itemDetails">
+          <h2 className="itemsContainer-itemsList-anItem-item-itemDetails-name">
+            {props.name}
+          </h2>
+          <p className="itemsContainer-itemsList-anItem-item-itemDetails-description">
+            {props.description}
+          </p>
+        </div>
+      </div>
+      <div className="itemsContainer-itemsList-details">
+        <h4>
+          <span>first name:</span> {props.deliveryDetails.firstName}
+        </h4>
+        <h4>
+          <span>last name:</span> {props.deliveryDetails.lastName}
+        </h4>
+        <h4>
+          <span>email:</span> {props.deliveryDetails.email}
+        </h4>
+        <h4>
+          <span>street:</span> {props.deliveryDetails.street}
+        </h4>
+        <h4>
+          <span>city:</span> {props.deliveryDetails.city}
+        </h4>
+        <h4>
+          <span>state:</span> {props.deliveryDetails.state}
+        </h4>
+        <h4>
+          <span>zip code:</span>
+          {props.deliveryDetails.zipCode}
+        </h4>
+      </div>
+      <div className="itemsContainer-itemsList-details-deliver">
         <div>
-          <h4>{props.deliveryDetails.firstName}</h4>
-          <h4>{props.deliveryDetails.lastName}</h4>
-          <h4>{props.deliveryDetails.email}</h4>
-          <h4>{props.deliveryDetails.street}</h4>
-          <h4>{props.deliveryDetails.city}</h4>
-          <h4>{props.deliveryDetails.state}</h4>
-          <h4>{props.deliveryDetails.zipCode}</h4>
+          <IoPrismOutline />
         </div>
       </div>
     </div>
