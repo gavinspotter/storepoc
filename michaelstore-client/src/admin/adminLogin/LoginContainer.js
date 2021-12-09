70adb39769c7615159548bdf75cecc966443e205
Osiris <potterosiris@gmail.com>
2021-12-09 12:48:43 -0500

    finished

--- michaelstore-client/src/admin/adminLogin/LoginContainer.js
+++ michaelstore-client/src/admin/adminLogin/LoginContainer.js
@@ -1,72 +1,60 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
-import {useHttpClient} from "../../shared/hooks/http-hook"
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
-import {AuthContext} from "../../shared/context/auth-context"
-import ErrorModal from "../../shared/UIElements/ErrorModal"
+import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/UIElements/ErrorModal";
 
-import "../../css/style.css"
+import "../../css/style.css";
 
 const LoginContainer = () => {
+  const auth = useContext(AuthContext);
 
-    const auth = useContext(AuthContext)
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-    const { isLoading, error, sendRequest, clearError } = useHttpClient()
+  const { register, handleSubmit } = useForm();
 
-    const { register, handleSubmit } = useForm()
-
-
-    const loginSubmit = async (data) => {
-
-        try {
-            const responseData = await sendRequest(
-                `${process.env.REACT_APP_BACKEND_URL}/admin/login`,
-                "POST",
-                JSON.stringify({
-                    username: data.username,
-                    password: data.password,
-                    
-                }),
-                {
-                    "Content-Type": "application/json"
-                }
-            )
-            auth.login(responseData.userId, responseData.token)
-        } catch (err) {
-
+  const loginSubmit = async (data) => {
+    try {
+      const responseData = await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/admin/login`,
+        "POST",
+        JSON.stringify({
+          username: data.username,
+          password: data.password,
+        }),
+        {
+          "Content-Type": "application/json",
         }
+      );
+      auth.login(responseData.userId, responseData.token);
+    } catch (err) {}
+  };
 
-
-    }
-
-
-
-    return (
-        <div className="michaelLogin">
-        <div className="michaelLogin-background">
-        <ErrorModal 
-        error={error} onClear={clearError}
-        />
+  return (
+    <div className="michaelLogin">
+      <div className="michaelLogin-background">
+        <ErrorModal error={error} onClear={clearError} />
 
         <form onSubmit={handleSubmit(loginSubmit)}>
-
-            <div className="michaelLogin-input-top">
+          <div className="michaelLogin-input-top">
             <label className="michaelLogin-input">😁</label>
             <label className="michaelLogin-input">username</label>
-            <input className="michaelLogin-input" {...register("username")}/>
+            <input className="michaelLogin-input" {...register("username")} />
             <label className="michaelLogin-input">password</label>
-            <input className="michaelLogin-input" {...register("password")} type="password"/>
-            <button>login</button>
-
-            </div>
-            
+            <input
+              className="michaelLogin-input"
+              {...register("password")}
+              type="password"
+            />
+            <button className="michaelLogin-button">login</button>
+          </div>
         </form>
-        
+      </div>
     </div>
-    </div>
-    )
-}
+  );
+};
 
-export default LoginContainer
+export default LoginContainer;
